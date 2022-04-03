c066917e112ab5d93d2963965b872063b8e4ac76
Lisa Brown <lisaann493@verizon.net>
2022-04-02 20:09:16 -0400

    fixed card details

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,29 +1,28 @@
 import Card from 'react-bootstrap/Card'
-import background from '../images/instacart_background.jpeg'
+import background from '../images/instacart_background.webp'
 
 const backgroundStyle = {
     backgroundSize: 'contain', 
     backgroundRepeat: 'no-repeat', 
     position: 'absolute',
-    right: '5',
-    top: '5',
-    left: '0',
+    right: '0',
+    top: '7em',
     zIndex: '-1', 
-    padding: '5em',
+    padding: '10em',
     height: '70%', 
     width: '100%', 
-    // backgroundPosition: 'center right', 
+    backgroundPosition: 'center right', 
     backgroundColor: '#deedd6'
 }
 
 function BackgroundImage() {
     return (
             <Card className='text-black' style={backgroundStyle} >
-                <Card.Img src={background} alt="produce" height='100%' width='100%' />
+                <Card.Img src={background} alt="produce" height='347px' width='428px' />
                 <Card.ImgOverlay>
-                    <Card.Title>
-                        Order groceries for delivery or pickup today
-                    </Card.Title>
+                    <Card.Text>
+                        <h1><strong>Order groceries for delivery or pickup today</strong></h1>
+                    </Card.Text>
                     <Card.Text>
                         Whatever you want from local stores, brought right to your door. 
                     </Card.Text>
